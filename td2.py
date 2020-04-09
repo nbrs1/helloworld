26c657d408ca35cf8c24362ead33a523a6652bdd
nbrs1 <nicolas.boursin@ensta-paris.fr>
2020-04-09 15:54:34 +0200

    ajouter part2 en branche

---
+++ td2.py
@@ -0,0 +1,34 @@
+class car
+    def __init__(self,type):
+        self.marque=marque
+        
+class berlin
+    def __init__(self,maxspeed,places,length,autonomy,automaticité,confort):
+        self.maxspeed=maxspeed
+        self.places=places
+        self.autonomy=autonomy
+        self.marque=marque
+        self.automaticité=automaticité
+    def deplacement(self,ville1,ville2):
+        if ((ville1[0]-ville2[0])**2+(ville[1]-ville[2])**2)**(1/2)<self.autonomy:
+            return true
+        else:
+            return false
+    def dureetrajet(self,v1,v2):
+        return ((ville1[0]-ville2[0])**2+(ville[1]-ville[2])**2)**(1/2)/self.maxspeed
+        
+class quatrequatre
+    def __init__(self,places,autonomy,taillepneu,confort,puissance):
+        self.places=places
+        self.autonomy=autonomy
+        self.taillepneu=taillepneu
+        self.confort=confort
+        self.puissance=puissance
+    def sensationenmontagne(self):
+        return self.confort*self.puissance
+    def deplacement(self,ville1,ville2):
+        if ((ville1[0]-ville2[0])**2+(ville[1]-ville[2])**2)**(1/2)<self.autonomy:
+            return true
+        else:
+            return false
+       
