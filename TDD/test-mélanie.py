a2cdbad29da1e63d94a8e69d7a6adb96c958eb8f
melanie-ensta <melanie.mille@ensta-paris.fr>
2020-04-16 22:05:46 +0200

    nouveau

--- TDD/test-mélanie.py
+++ TDD/test-mélanie.py
@@ -1,16 +1,22 @@
-from tp2 import berlin
+import td2
 import unittest
 
 
 
-ville1=[0,1]
-ville2=[0,2]
-voiture=berlin(100,5,6,5,1,6)
+ville=[[10,1],[0,5],[-5,20],[2,25]]
+
+voiture=td2.berlin(100,5,6,5,1,6)
 
 
 
 class Test(unittest.TestCase):
 	def test_trajet_possible(self):
+		for i in range(2):
+			result=voiture.trajet_possible(self.ville[i],self.ville[i+1])
+			rtest=False
+			if ((self.ville[i][0]-self.ville[i+1][0])**2+(self.ville[i][1]-self.ville[i+1][1])**2)**(1/2)<self.autonomy:
+				rtest=True
+			self.assertEqual(result,rtest)
 		
 		assert(trajet_possible(voiture,ville1,ville2))
 	def test_autonomy_sign(self):
