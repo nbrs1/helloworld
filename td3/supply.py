1e275b1d91008a53d44045844106ef42e97bc572
melanie-ensta <melanie.mille@ensta-paris.fr>
2020-05-08 16:35:13 +0200

    ok

--- td3/supply.py
+++ td3/supply.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy.stats import pearsonr
-import sklearn.metrics as metrics
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import random as rd 
 from math import sqrt
@@ -17,11 +17,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from scipy.special import expit
-<<<<<<< HEAD
-
-sns.set()
-#premiere etape: creer le dictionnaire a partir du xlsx
-=======
 sns.set()
 
 
@@ -29,10 +24,9 @@ sns.set()
 #premiere etape: creer le dictionnaire a partir du xlsx
 
 
->>>>>>> 11348414bec1635dae2caa41fe93cabd4b6cd422
 storage_data=pd.read_excel("storage_datarealone.xlsx",sheet_name=None)
 
-print(storage_data["SF - UGS Peckensen"].columns)
+
 
 #2e étape : Calculate a net withdrawal column
 for key in storage_data:
@@ -41,14 +35,22 @@ for key in storage_data:
 	l=[]
 	for i in range(len(inj)):
 		l.append(wit[i]-inj[i])
+
 	storage_data[key]["NW"]=pd.DataFrame(l)
+
+
+
 #3e étape : Calculate a lagged net withdrawal column of day prior
+for key in storage_data:
 	l1=storage_data[key]["NW"].values
 	l2=[0]  #le jour le premier jour  on sait pas donc on met 0
 	for i in range(len(l1)-1):
 		l2.append(l1[i])
 	storage_data[key]["lagged_NW"]=pd.DataFrame(l2)
+#print(storage_data["SF - UGS Peckensen"])
+
 #Create a new Net Withdrawal column named Net Withdrawal_binary
+for key in storage_data:
 	l1=storage_data[key]["NW"].values
 	l2=[]
 	for i in range(len(l1)):
@@ -57,8 +59,10 @@ for key in storage_data:
 		else :
 			l2.append(0)
 	storage_data[key]["Net Withdrawal_binary"]=pd.DataFrame(l2)
+
 #FSW1 = max(Full Stock - 45, 0)
 #FSW2 = max(45 - Full Stock, 0)
+for key in storage_data:
 	l=storage_data[key]["full"].values
 	l1=[]
 	l2=[]
@@ -69,53 +73,30 @@ for key in storage_data:
 	storage_data[key]["FSW2"]=pd.DataFrame(l2)
 
 price_data = pd.read_csv("price_data.csv", sep=";")
-price_data.rename(columns={"Date": "gasDayStartedOn"}, inplace=True)	
+price_data.rename(columns={"Date": "gasDayStartedon"}, inplace=True)
+
+
+#print(price_data)	
+#print(storage_data["SF - UGS Peckensen"])
+
 #Part 1: Classification
 #dictionnaries to put the results of the 2 models
-Logistic_Regression={}
-random_forest={}
+model1={}
+model2={}
 #inner join
-#print(storage_data["SF - UGS Peckensen"]["gasDayStartedOn"])
-print(price_data["gasDayStartedOn"])
-data={}
-for key in storage_data:
-	data[key]=pd.merge(storage_data[key],price_data, on="gasDayStartedOn")
-
-
-
+"""data={}
+# for key in storage_data:
+# 	data[key]=pd.merge(storage_data[key],price_data)"""
 # logistic regression
 #Your y array is the Net Withdrawal_binary column
 #Your X matrix is composed of the Lagged_NW, FSW1, FSW2 and all the time spreads price columns
-<<<<<<< HEAD
-X=price_data
-del X['gasDayStartedon']
-=======
 
 # X=price_data
 # del X['gasDayStartedon']
->>>>>>> 11348414bec1635dae2caa41fe93cabd4b6cd422
 
 # for key in storage_data:
 # 	y=np.array(storage_data[key]["Net Withdrawal_binary"].values)
 	
-<<<<<<< HEAD
-	X["lagged_NW"]=storage_data[key]["lagged_NW"]
-	X["FSW1"]=storage_data[key]["FSW1"]
-	X["FSW2"]=storage_data[key]["FSW2"]	
-	x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=1)
-	lr = LogisticRegression()
-	Logi=lr.fit(x_train, y_train)
-	y_pred = lr.predict(x_test)
-	cm=confusion_matrix(y_test, y_pred)
-	Logistic_Regression[key]={"recall": metrics.recall_score(y_test, y_pred), "neg_recall": cm[1,1]/(cm[0,1] + cm[1,1]), "confusion": cm,"precision": metrics.precision_score(y_test, y_pred),"neg_precision":cm[1,1]/cm.sum(axis=1)[1],"roc": metrics.roc_auc_score(y_test, probs)}
-print(Logistic_Regression)													
-
-#storage_data=pd.read_excel("storage_data(1).xlsx",sheet_name=None)
-
-#print(storage_data['SF -UGS Rehden'])
-
-
-=======
 # 	X["lagged_NW"]=storage_data[key]["lagged_NW"]
 # 	X["FSW1"]=storage_data[key]["FSW1"]
 # 	X["FSW2"]=storage_data[key]["FSW2"]
@@ -152,9 +133,9 @@ rmodel={}
 
 #multidimensionnal regression
 
-print(storage_data["SF - UGS Peckensen"].describe())
+# print(storage_data["SF - UGS Peckensen"])
 
 
 
 
->>>>>>> 11348414bec1635dae2caa41fe93cabd4b6cd422
+
